ea711dff6356a6073d159457bf3cbdc7fdd30f7e
mrcormell <d.cormell@etoncollege.org.uk>
2021-02-24 14:50:35 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by David Cormell on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
