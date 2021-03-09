e77e3f315e097da65f0bb576ea375210e9181bff
mrcormell <d.cormell@etoncollege.org.uk>
2021-03-09 16:49:59 +0000

    Using generic File Manager extension methods end of video 15

--- AbsenceRecorder/StateController.swift
+++ AbsenceRecorder/StateController.swift
@@ -15,38 +15,12 @@ class StateController: ObservableObject {
     }
     
     func loadFromFile() {
-        //get url to the file you want to load from
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let userPath = paths[0]
-        let url = userPath.appendingPathComponent("divisions.json")
-        if let data = try? Data(contentsOf: url) {
-            let decoder = JSONDecoder()
-            if let loaded = try? decoder.decode([Division].self, from: data) {
-                divisions = loaded
-            }
+        if let loaded: [Division] = FileManager.default.load(from: "divisions.json") {
+            divisions = loaded
         }
-        
     }
     
     func saveToFile() {
-        //create json encoder
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(divisions) {
-            //take encoded json objects and convert them into string
-            if let json = String(data: encoded, encoding: .utf8) {
-                //create the file path
-                let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-                let userPath = paths[0]
-                let url = userPath.appendingPathComponent("divisions.json")
-                do {
-                    //write the json string to that file
-                    try json.write(to: url, atomically: true, encoding: .utf8)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            } else {
-                print("unable to form json string")
-            }
-        }
+        FileManager.default.save(to: "divisions.json", object: divisions)
     }
 }
